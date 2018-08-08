8b3974167ae68b5ddfcf5374730e8b6742796878
ta1nakamura <ta1nakamura@gmail.com>
2018-08-08 11:47:15 +0700

    add getCurrentLocation in Place edit

--- pages/place/_id/index.vue
+++ pages/place/_id/index.vue
@@ -26,7 +26,11 @@
       v-model="inputaddress"></v-text-field>
     </v-flex>
 </v-layout>
-<v-btn @click="onGeocode" color="secondary">goMap</v-btn>
+
+<v-btn @click="onGeocode" color="secondary">goAddress</v-btn>
+<v-btn flat icon @click="onClickCurrent" color="secondary">
+  <v-icon>gps_fixed</v-icon>
+</v-btn>
 <v-btn @click="onSave" color="indigo">save</v-btn>
 
 <!-- [output] -->
@@ -177,8 +181,45 @@ export default {
       // this.$forceUpdate()
       
     },
+    async onClickCurrent(){
+            console.log('--onClickCurrent')
+            try{
+                
+                let data =await getPosition();
+                console.log('--success',data)
+                console.log('typeof', typeof(data))
+                
+                let data2={};
+                data2.lat = data.coords.latitude ;
+                data2.lng = data.coords.longitude ;
+                data2.alt = data.coords.altitude ;
+                data2.accLatlng = data.coords.accuracy ;
+                data2.accAlt = data.coords.altitudeAccuracy ;
+                data2.heading = data.coords.heading ;			//0=北,90=東,180=南,270=西
+                data2.speed = data.coords.speed ;
 
+                // Object.assign(data2,data.coords); // not work
+                // this.$set(this.currentLoc,"data2",data.coords);//not work
+                console.log('data2',data2)
+                // this.$set(this.currentLoc,"coords",data2);
+
+                //set Googlemap
+                this.maplocation.lat = data2.lat;
+                this.maplocation.lng = data2.lng;
+                this.reportedCenter.lat = data2.lat;
+                this.reportedCenter.lng = data2.lng;
+                this.$refs.mmm.panTo(this.maplocation );
+                
+            }catch(e){
+                console.log('--error',e);
+            }
+        }
   }
+};
+var getPosition = function (options) {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject, options);
+  });
 };
 </script>
 
